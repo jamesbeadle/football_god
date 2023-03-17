318ca13c7a4348a18674125a38678390a6878f86
James Beadle <jamesbeadle1989@gmail.com>
2023-03-17 00:05:37 +0000

    Init Commit

---
+++ src/football_god_frontend/src/index.jsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import { render } from "react-dom";
+import { football_god_backend } from "../../declarations/football_god_backend";
+
+const MyHello = () => {
+  const [name, setName] = React.useState('');
+  const [message, setMessage] = React.useState('');
+
+  async function doGreet() {
+    const greeting = await football_god_backend.greet(name);
+    setMessage(greeting);
+  }
+
+  return (
+    <div style={{ "fontFamily": "sans-serif" }}>
+    <div style={{ "fontSize": "30px" }}>
+      <p>Greetings, from DFINITY!</p>
+      <p>
+        {" "}
+        Type your message in the Name input field, then click{" "}
+        <b> Get Greeting</b> to display the result.
+      </p>
+    </div>
+    <div style={{ margin: "30px" }}>
+      <input
+        id="name"
+        placeholder="Type text here"
+        value={name}
+        onChange={(ev) => setName(ev.target.value)}
+      ></input>
+      <button onClick={doGreet}>Get Greeting!</button>
+    </div>
+    <div>
+      Greeting is: "
+      <span style={{ color: "green" }}>{message}</span>"
+    </div>
+  </div>
+  );
+};
+
+render(<MyHello />, document.getElementById("app"));
