1505c52688e9ee9e863f04a5d5d94ef2417e4fb2
James Beadle <jamesbeadle1989@gmail.com>
2023-03-20 21:06:29 +0000

    Added local storage expiry

--- src/football_god_frontend/src/components/shared/navbar.jsx
+++ src/football_god_frontend/src/components/shared/navbar.jsx
@@ -1,52 +1,27 @@
-import React, { useState, useContext  } from "react";
+import React, { useState, useContext, useEffect  } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
-import { Actor } from "@dfinity/agent";
 import { Navbar, Nav, NavDropdown, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { football_god_backend as football_god_backend_actor } from '../../../../declarations/football_god_backend';
 
 const MyNavbar = () => {
-  const [isConnected, setIsConnected] = useState(false);
-  const [isAdmin, setIsAdmin] = useState(false);
-
-  const authClient = useContext(AuthContext);
-
-  const handleConnect = async () => {
-
-    await authClient.login({
-      identityProvider: process.env.II_URL,
-      onSuccess: async () => {
-        const identity = authClient.getIdentity();
-        Actor.agentOf(football_god_backend_actor).replaceIdentity(identity);
-        const userIsAdmin = await football_god_backend_actor.isAdmin();
-        setIsAdmin(userIsAdmin);
-        setIsConnected(true);
-      }
-    });
-  };
-
-  const handleDisconnect = () => {
-    setIsConnected(false);
-    setIsAdmin(false);
-    authClient.logout();
-  };
+  const { isAdmin, isAuthenticated, login, logout } = useContext(AuthContext);
 
   return (
     <Navbar bg="primary" variant="dark" expand="lg">
-      <Navbar.Brand  as={Link} to="/">Football God</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/">Football God</Navbar.Brand>
       <Nav className="ml-auto">
-        {isConnected ? (
+        {isAuthenticated ? (
           <>
             {isAdmin && <Nav.Link as={Link} to="/admin">Admin</Nav.Link>}
             <Nav.Link href="#history">History</Nav.Link>
             <NavDropdown title="My Account" id="basic-nav-dropdown" alignRight>
               <NavDropdown.Item href="#profile">Profile</NavDropdown.Item>
               <NavDropdown.Divider />
-              <Button onClick={handleDisconnect}>Disconnect</Button>
+              <NavDropdown.Item onClick={logout}>Disconnect</NavDropdown.Item>
             </NavDropdown>
           </>
         ) : (
-          <Button onClick={handleConnect}>Connect</Button>
+          <Button onClick={login}>Connect</Button>
         )}
       </Nav>
     </Navbar>
--- src/football_god_frontend/src/contexts/AuthContext.js
+++ src/football_god_frontend/src/contexts/AuthContext.js
@@ -1,21 +1,104 @@
 import React, { useState, useEffect } from 'react';
 import { AuthClient } from "@dfinity/auth-client";
+import { Actor } from "@dfinity/agent";
+import { football_god_backend as football_god_backend_actor } from '../../../declarations/football_god_backend';
 
 export const AuthContext = React.createContext();
 
 export const AuthProvider = ({ children }) => {
   const [authClient, setAuthClient] = useState(null);
-
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [expiryTime, setExpiryTime] = useState(null);
+ 
   useEffect(() => {
     const initAuthClient = async () => {
       const authClient = await AuthClient.create();
+
+      const storedIdentityStr = localStorage.getItem('identity');
+      if (storedIdentityStr) {
+        setIsAuthenticated(true);
+      }
+
+      const storedIsAdmin = localStorage.getItem('isAdmin');
+      if (storedIsAdmin !== null) {
+        setIsAdmin(storedIsAdmin === 'true');
+      }
+
       setAuthClient(authClient);
+
+      const storedExpiryTime = localStorage.getItem('expiryTime');
+      if (storedExpiryTime) {
+        setExpiryTime(Number(storedExpiryTime));
+      }
     };
     initAuthClient();
   }, []);
 
+  useEffect(() => {
+    if (expiryTime) {
+      const timer = setTimeout(() => {
+        logout();
+      }, expiryTime - Date.now());
+
+      return () => clearTimeout(timer);
+    }
+  }, [expiryTime]);
+
+  useEffect(() => {
+    const checkExpiry = () => {
+      const expiryTime = localStorage.getItem('expiryTime');
+      if (expiryTime && Date.now() > Number(expiryTime)) {
+        setIsAuthenticated(false);
+        setIsAdmin(false);
+        localStorage.removeItem('identity');
+        localStorage.removeItem('isAdmin');
+        localStorage.removeItem('expiryTime');
+        authClient.logout();
+      }
+    };
+  
+    const intervalId = setInterval(checkExpiry, 60 * 1000); // Check every minute
+  
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [authClient]);
+
+  const login = async () => {
+    await authClient.login({
+      identityProvider: process.env.II_URL,
+      onSuccess: async () => {
+        const identity = authClient.getIdentity();
+        Actor.agentOf(football_god_backend_actor).replaceIdentity(identity);
+        const userIsAdmin = await football_god_backend_actor.isAdmin();
+        setIsAdmin(userIsAdmin);
+        setIsAuthenticated(true);
+        localStorage.setItem('identity', JSON.stringify(identity));
+        localStorage.setItem('isAdmin', userIsAdmin);
+
+        const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
+        const expirationTime = Date.now() + oneDayInMilliseconds;
+        localStorage.setItem('expiryTime', expirationTime);
+        setExpiryTime(expirationTime);
+
+      }
+    });
+  };
+
+  const logout = () => {
+    setIsAuthenticated(false);
+    setIsAdmin(false);
+    authClient.logout();
+    localStorage.removeItem('identity');
+    localStorage.removeItem('isAdmin');
+    localStorage.removeItem('expiryTime');
+    setExpiryTime(null);
+  };
+
+
   return (
-    <AuthContext.Provider value={authClient}>
+    <AuthContext.Provider value={ { authClient, isAdmin, isAuthenticated, setIsAdmin, setIsAuthenticated, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
