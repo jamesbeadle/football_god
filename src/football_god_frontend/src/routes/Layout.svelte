528eed1f4ac973813ab299f6d6c126f7af8f1c77
jamesbeadle <jamesbeadle1989@gmail.com>
2024-03-24 23:00:44 +0000

    Update layout

--- src/football_god_frontend/src/routes/Layout.svelte
+++ src/football_god_frontend/src/routes/Layout.svelte
@@ -1,22 +1,20 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import { fade } from "svelte/transition";
   import { browser } from "$app/environment";
+  import { initAuthWorker } from "$lib/services/worker.auth.services";
   import { authStore, type AuthStoreData } from "$lib/stores/auth.store";
   import { toastsError } from "$lib/stores/toasts.store";
-  import "../app.css";
   import { writable } from 'svelte/store';
-
-  import { slide } from 'svelte/transition';
   import { BusyScreen, Spinner, Toasts } from "@dfinity/gix-components";
-  import { initAuthWorker } from "$lib/services/worker.auth.services";
-    import LogoIcon from "$lib/icons/LogoIcon.svelte";
-    import HomeIcon from "$lib/icons/HomeIcon.svelte";
-    import BettingIcon from "$lib/icons/BettingIcon.svelte";
-    import GamesIcon from "$lib/icons/GamesIcon.svelte";
-    import StarIcon from "$lib/icons/StarIcon.svelte";
-    import RulesIcon from "$lib/icons/RulesIcon.svelte";
-
+  import LogoIcon from "$lib/icons/LogoIcon.svelte";
+  import HomeIcon from "$lib/icons/HomeIcon.svelte";
+  import BettingIcon from "$lib/icons/BettingIcon.svelte";
+  import GamesIcon from "$lib/icons/GamesIcon.svelte";
+  import StarIcon from "$lib/icons/StarIcon.svelte";
+  import RulesIcon from "$lib/icons/RulesIcon.svelte";
+  import { fade } from "svelte/transition";
+  import "../app.css";
+  
   const init = async () => await Promise.all([syncAuthStore()]);
 
   let isExpanded = writable(false);
@@ -67,80 +65,84 @@
 
 <div class="flex h-screen">
   <div
-    class:slide={$isExpanded}
-    class:bg-gray-800={$isExpanded}
-    class:text-white={$isExpanded}
-    class:flex={$isExpanded}
-    class:flex-col={$isExpanded}
-    class:items-center={$isExpanded}
-    class:justify-between={$isExpanded}
-    class:py-4={$isExpanded}
-    class:w-16={!$isExpanded}
-    class:w-48={$isExpanded}
-    transition:slide={{ duration: 300 }}>
-    <button on:click={() => $isExpanded = !$isExpanded} class="mb-4">
-      <!-- Replace with your expand/collapse icon -->
-      <span>{$isExpanded ? '<<' : '>>'}</span>
-    </button>
-    
-    <div class="p-5 text-gray-400">
-      <div class="flex flex-row text-xl items-center text-white">
-        <LogoIcon className="w-6 mr-2 my-4" /> 
-        {#if $isExpanded}<span>FootballGod</span>{/if}
-      </div>
-      <a href="/" class="block mt-4 text-lg">
-        <div class="flex flex-row items-center">
-          <HomeIcon className="w-6 mr-2" /> 
-          {#if $isExpanded}<span>Home</span>{/if}
-        </div>
-      </a>
-      <a href="/" class="block mt-4 text-lg">
-        <div class="flex flex-row items-center">
-          <BettingIcon className="w-6 mr-2" /> 
-          {#if $isExpanded}<span>Match Betting</span>{/if}
-        </div>
-      </a>
-      <a href="/" class="block mt-4 text-lg">
-        <div class="flex flex-row items-center">
-          <GamesIcon className="w-6 mr-2" /> 
-          {#if $isExpanded}<span>Mini Games</span>{/if}
-        </div>
-      </a>
-      <a href="/euro2024" class="block mt-4 text-lg">
-        <div class="flex flex-row items-center">
-          <StarIcon className="w-6 mr-2" /> 
-          {#if $isExpanded}<span>Euro 2024</span>{/if}
-        </div>
-      </a>
-      <a href="/" class="block mt-4 text-lg">
-        <div class="flex flex-row items-center">
-          <RulesIcon className="w-6 mr-2" /> 
-          {#if $isExpanded}<span>Rules</span>{/if}
-        </div>
-      </a>
-    </div>
-    
-    <!-- Bottom Section, if needed -->
-    <div>
-      {#if $isExpanded}<span>User</span>{/if}
-    </div>
-  </div>
+    class="bg-gray-800 text-white flex flex-col justify-between transition-width duration-300 p-5"
+    style="width: {$isExpanded ? '16rem' : '4rem'}">
+      <div>
+        <button on:click={() => $isExpanded = !$isExpanded} class="mb-4">
+          <span>{$isExpanded ? '<<' : '>>'}</span>
+        </button>
   
-  <!-- Main content area -->
-  <div class="flex-1">
-    <p>Main content goes here. Click icons to navigate.</p>
-  </div>
+        <div class="text-gray-400">
+          <!-- The logo will be here, right below the expand button -->
+          <div class="flex flex-row text-xl items-center text-white">
+            <LogoIcon className="w-6 mr-2 my-4" />
+            {#if $isExpanded}
+              <span in:fade={{ delay: 100, duration: 200 }} out:fade={{ delay: 0, duration: 100 }}>FootballGod</span>
+            {/if}
+          </div>
+  
+          <!-- The rest of the navigation links -->
+          <a href="/" class="block mt-4 text-lg">
+            <div class="flex flex-row items-center">
+              <HomeIcon className="w-6 mr-2" />
+              {#if $isExpanded}
+                <span in:fade={{ delay: 100, duration: 200 }} out:fade={{ delay: 0, duration: 100 }}>Home</span>
+              {/if}
+            </div>
+          </a>
+          <a href="/" class="block mt-4 text-lg">
+            <div class="flex flex-row items-center">
+              <BettingIcon className="w-6 mr-2" /> 
+              {#if $isExpanded}
+              <span in:fade={{ delay: 100, duration: 200 }} out:fade={{ delay: 0, duration: 100 }}>Match Betting</span>
+              {/if}
+            </div>
+          </a>
+          <a href="/" class="block mt-4 text-lg">
+            <div class="flex flex-row items-center">
+              <GamesIcon className="w-6 mr-2" /> 
+              {#if $isExpanded}
+              <span in:fade={{ delay: 100, duration: 200 }} out:fade={{ delay: 0, duration: 100 }}>Mini Games</span>
+              {/if}
+            </div>
+          </a>
+          <a href="/euro2024" class="block mt-4 text-lg">
+            <div class="flex flex-row items-center">
+              <StarIcon className="w-6 mr-2" /> 
+              {#if $isExpanded}
+              <span in:fade={{ delay: 100, duration: 200 }} out:fade={{ delay: 0, duration: 100 }}>Euro 2024</span>
+              {/if}
+            </div>
+          </a>
+          <a href="/" class="block mt-4 text-lg">
+            <div class="flex flex-row items-center">
+              <RulesIcon className="w-6 mr-2" /> 
+              {#if $isExpanded}
+              <span in:fade={{ delay: 100, duration: 200 }} out:fade={{ delay: 0, duration: 100 }}>Rules</span>
+              {/if}
+            </div>
+          </a>
+        </div>
+      </div>
+  
+      <div class="mb-4">
+        {#if $isExpanded}
+          <span in:fade={{ delay: 100, duration: 200 }} out:fade={{ delay: 0, duration: 100 }}>User</span>
+        {/if}
+      </div>
+    </div>
+    
+    <div class="flex-1">
+      <p>Main content goes here. Click icons to navigate.</p>
+    </div>
 </div>
 {/await}
 
 <Toasts />
 <BusyScreen />
 
-
-
-
 <style>
-  .expanded {
-    @apply w-48;
+  .transition-width {
+    transition: width 300ms;
   }
 </style>
