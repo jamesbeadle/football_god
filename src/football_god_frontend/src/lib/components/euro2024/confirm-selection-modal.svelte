703e5ccb78e0e97d0c62c4378f8ba55755230f67
James Beadle <jamesbeadle1989@gmail.com>
2024-05-26 23:50:47 +0100

    Update save modal

--- src/football_god_frontend/src/lib/components/euro2024/confirm-selection-modal.svelte
+++ src/football_god_frontend/src/lib/components/euro2024/confirm-selection-modal.svelte
@@ -3,7 +3,7 @@
   import RefreshIcon from "$lib/icons/RefreshIcon.svelte";
     import { toastsError, toastsShow } from "$lib/stores/toasts.store";
     import { userStore } from "$lib/stores/user.store";
-import { Modal } from "@dfinity/gix-components";
+import { Modal, Spinner } from "@dfinity/gix-components";
   import { onMount } from "svelte";
   import type { AccountBalancesDTO } from "../../../../../declarations/football_god_backend/football_god_backend.did";
 
@@ -15,7 +15,6 @@ import { Modal } from "@dfinity/gix-components";
   let entryFee = 10_000_000_000n;
   let isLoading = true;
   
-
   onMount(async () => {
     try {
       await userStore.sync();
@@ -23,6 +22,7 @@ import { Modal } from "@dfinity/gix-components";
       if(userBalances){
         accountBalances = userBalances;
       }
+      console.log(userBalances)
 
       if(!userBalances){
         accountBalances = {
@@ -58,39 +58,49 @@ import { Modal } from "@dfinity/gix-components";
     }
   }
 
-  function refreshBalance () {
-    //TODO: Add refresh
+  async function refreshBalance () {
+    let userBalances = await userStore.getAccountBalances();
+    if(userBalances){
+      accountBalances = userBalances;
+    }
+    console.log(accountBalances)
   }
 </script>
 
 <Modal {visible} on:nnsClose={closeSelectionModal}>
-  <div class="px-8 pb-8 pt-4">
+  <div class="px-8 pb-4 pt-4">
     <div class="flex justify-between items-center">
       <h3 class="default-header py-4">Confirm Entry</h3>
       <button class="times-button" on:click={closeSelectionModal}>&times;</button>
     </div>  
-    {#if !isLoading}
     <div >
-      <button on:click={refreshBalance}>
+      {#if isLoading}
+        <p class="py-4">Please wait, checking your FPL balance.</p>
+      {:else}
+      <button on:click={refreshBalance} class="mb-4">
         <span class="flex flex-row items-center">
-          <p class="py-4">Balance: {accountBalances.fplBalance} FPL</p>
+          {#if isLoading}
+            <p class="py-4">Loading Balance</p>
+          {:else}
+            <p class="py-4">Balance: {(accountBalances.fplBalance / 100_000_000n)} FPL</p>
+          {/if}
           <RefreshIcon className="w-5 ml-2" />
         </span>
       </button>
-       {#if accountBalances.fplBalance < entryFee}
-        <p class="py-4">Please confirm you are happy to pay 100 FPL to enter the Euro 2024 sweepstake.</p>
-        <button on:click={confirmSubmit} class="w-full bg-OPENFPLPURPLE rounded-md mt-8 px-4 py-2">Pay 100 FPL & Enter</button>
-      {:else}
-        <p>You will need 100 FPL to enter this competition, please deposit to your principal ID {$userStore.principalId}:</p>
-        <button
-          class="flex items-center text-left text-xxs break-all"
-          on:click={() => copyAndShowToast($userStore.principalName)}
-        >
-          <span>{$userStore.principalName}</span>
-          <CopyIcon className="w-7 xs:w-6 text-left" fill="#FFFFFF" />
-        </button>
+        {#if accountBalances.fplBalance >= entryFee}
+          <p>Please confirm you are happy to pay 100 FPL to enter the Euro 2024 sweepstake.</p>
+          <button on:click={confirmSubmit} class="w-full bg-OPENFPLPURPLE rounded-md my-8 px-4 py-2">Pay 100 FPL & Enter</button>
+        {:else}
+          <p>You will need 100 FPL to enter this competition, please deposit to your principal ID:</p>
+          <button
+            class="flex items-center text-left text-sm break-all my-4 py-2"
+            on:click={() => copyAndShowToast($userStore.principalName)}
+          >
+            <span>{$userStore.principalName}</span>
+            <CopyIcon className="w-7 xs:w-6 text-left" fill="#FFFFFF" />
+          </button>
+        {/if}
       {/if}
     </div>
-    {/if}
   </div>  
 </Modal>
