753b282cfebdc6a8ce13f3efc5379b74e4fa94b8
jamesbeadle <jamesbeadle1989@gmail.com>
2025-04-13 09:27:05 +0100

    Add copy principal component and update profile layout

--- src/frontend/src/lib/components/profile/profile-detail.svelte
+++ src/frontend/src/lib/components/profile/profile-detail.svelte
@@ -2,31 +2,23 @@
   import { onMount } from "svelte";
   import { writable } from "svelte/store";
   import { userStore } from "$lib/stores/user-store";
-  import { authStore } from "$lib/stores/auth-store";
-  import { toasts } from "$lib/stores/toasts-store";
   
-  import CopyIcon from "$lib/icons/CopyIcon.svelte";
-  import FullScreenSpinner from "../shared/full-screen-spinner.svelte";
   import WithdrawIcfcModal from "./withdraw-icfc-modal.svelte";
-    import IcfcCoinIcon from "$lib/icons/ICFCCoinIcon.svelte";
-   
-  let isLoading = true;
-  let loadingBalances = true;
+  import IcfcCoinIcon from "$lib/icons/ICFCCoinIcon.svelte";
+  import CopyPrincipal from "./copy-principal.svelte";
+  
+  let isLoading = $state(true);
 
   let icfcBalance = 0n;
-  let icfcBalanceFormatted = "0.0000"; 
+  let icfcBalanceFormatted = $state("0.0000"); 
   let dots = writable('.');
   let dot_interval: ReturnType<typeof setInterval>;
-  let principalId = "";
 
-  let showICFCModal = false;
+  let showICFCModal = $state(false);
 
   onMount(async () => {
     try {
       startDotAnimation();
-      await authStore.sync();
-      principalId = $authStore.identity?.getPrincipal().toText() ?? "";
-      isLoading = false;
       await fetchBalances();
     } catch (error) {
       console.error("Error fetching profile detail:", error);
@@ -52,8 +44,6 @@
     await fetchBalances();
   };
 
-  
-
   async function fetchBalances() {
     try {
       let icfcBalance = await userStore.getICFCBalance();
@@ -62,28 +52,30 @@
       console.error("Error fetching profile detail:", error);
     } finally {
       clearInterval(dot_interval);
-      loadingBalances = false;
-    }
-  }
-
-  async function copyTextAndShowToast(text: string) {
-    try {
-      await navigator.clipboard.writeText(text);
-      toasts.addToast({
-        type: "success",
-        message: "Copied to clipboard.",
-        duration: 2000,
-      });
-    } catch (err) {
-      console.error("Failed to copy:", err);
+      isLoading = false;
     }
   }
 
 </script>
 
-{#if isLoading}
-  <FullScreenSpinner />
-{:else}
+<div class="panel-content">
+  <CopyPrincipal />
+  {#if isLoading}
+    <div class="dot-animation min-w-[20px]">{$dots}</div>
+  {:else}
+  <p class="stacked-row items-center">
+    <IcfcCoinIcon className="w-6 mr-2" />
+    {icfcBalanceFormatted} ICFC
+  </p>
+  <div class="flex items-center text-xs mt-2">
+    <button class="brand-button" onclick={loadWithdrawICFCModal}>
+      Withdraw
+    </button>
+  </div>
+    {/if}
+</div>
+
+{#if showICFCModal}
   <WithdrawIcfcModal
     visible={showICFCModal}
     closeModal={closeWithdrawICFCModal}
@@ -91,70 +83,4 @@
     icfcBalance={icfcBalance}
     icfcBalanceFormatted={icfcBalanceFormatted}
   />
-
-
-  
-  <div class="container mt-4 mx-6">
-    <div class="flex flex-wrap">
-      <div class="w-full mb-4 md:mb-0">
-        <div class="mt-2 md:mt-1 rounded-lg">
-          <p class="mb-1 mt-4 text-xs">Principal:</p>
-          <div class="flex items-center">
-            <button
-              class="flex items-center text-left text-xxs break-all"
-              on:click={() => copyTextAndShowToast(principalId)}
-            >
-              <span>{ principalId }</span>
-              <CopyIcon className="w-7 xs:w-6 text-left" fill="#FFFFFF" />
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="flex flex-wrap">
-      <div class="w-full mb-4">
-        <div class="mt-4">
-          <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <div
-              class="flex items-center p-4 rounded-lg shadow-md border border-gray-700"
-            >
-            <IcfcCoinIcon className="h-12 w-12 md:h-9 md:w-9" />
-              <div class="ml-4 md:ml-3">
-
-                  {#if loadingBalances}
-                    <div class="dot-animation min-w-[20px]">{$dots}</div>
-                  {:else}
-                  <p>
-                    {icfcBalanceFormatted} ICFC
-                  </p>
-                  {/if}
-
-                <div class="flex items-center text-xs">
-                  <button
-                    class="flex items-center text-left break-all"
-                    on:click={() => copyTextAndShowToast(principalId)}
-                  >
-                    <span>{principalId}</span>
-                    <CopyIcon className="w-7 xs:w-6 text-left" fill="#FFFFFF" />
-                  </button>
-                </div>
-
-
-                {#if !loadingBalances}
-                  <div class="flex items-center text-xs mt-2">
-                    <button
-                    class="text-sm md:text-sm p-1 md:p-2 px-2 md:px-4 rounded fg-button button-hover"
-                      on:click={loadWithdrawICFCModal}
-                    >
-                      Withdraw
-                    </button>
-                  </div>
-                {/if}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
 {/if}
